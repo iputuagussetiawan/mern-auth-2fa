5cbad281906e18bdcf45f4c68618e9b0c05e7d5b
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-01-22 19:08:37 +0800

    login api and refresh token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Request, Response } from "express";
 import { asyncHandler } from "../../middlewares/asyncHandler";
 import { AuthService } from "./auth.service";
 import { HTTPSTATUS } from "../../config/http.config";
-import { registerSchema } from "../../common/validators/auth.validator";
+import { loginSchema, registerSchema } from "../../common/validators/auth.validator";
+import { getAccessTokenCookieOptions, getRefreshTokenCookieOptions, setAuthenticationCookies } from "../../common/utils/cookie";
+import { UnauthorizedException } from "../../common/utils/catch-errors";
 
 export class AuthController {
     private authService: AuthService
@@ -22,5 +24,47 @@ export class AuthController {
             data:user
         })
     })
+
+    public login = asyncHandler(async (req: Request, res: Response): Promise<any> => {
+        const userAgent=req.headers["user-agent"];
+        const body=loginSchema.parse({
+            ...req.body,
+            userAgent
+        });
+        const {user, accessToken, refreshToken, mfaRequired}=await this.authService.login(body); 
+        
+        return setAuthenticationCookies({res, accessToken, refreshToken}).status(HTTPSTATUS.OK).json({
+            message:"User Login Successfully",
+            mfaRequired,
+            user
+        })
+    });
+
+    public refreshToken = asyncHandler(
+        async (req: Request, res: Response): Promise<any> => {
+            const refreshToken = req.cookies.refreshToken as string | undefined;
+            if (!refreshToken) {
+                throw new UnauthorizedException("Missing refresh token");
+            }
+
+            const { accessToken, newRefreshToken } =
+            await this.authService.refreshToken(refreshToken);
+
+            if (newRefreshToken) {
+                res.cookie(
+                    "refreshToken",
+                    newRefreshToken,
+                    getRefreshTokenCookieOptions()
+                );
+            }
+
+            return res
+            .status(HTTPSTATUS.OK)
+            .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+            .json({
+                message: "Refresh access token successfully",
+            });
+        }
+    );
 
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,17 @@
 import e from "express";
-import { RegisterDto } from "../../common/interface/auth.interface";
-import { BadRequestException } from "../../common/utils/catch-errors";
+import { LoginDto, RegisterDto } from "../../common/interface/auth.interface";
+import { BadRequestException, UnauthorizedException } from "../../common/utils/catch-errors";
 import UserModel from "../../database/models/user.model";
 import { ErrorCode } from "../../common/enums/error-code.enum";
 import { VerificationEnum } from "../../common/enums/verification-code.enum";
 import VerificationCodeModel from "../../database/models/verification.model";
-import { fortyFiveMinutesFromNow } from "../../common/utils/date-time";
+import { calculateExpirationDate, fortyFiveMinutesFromNow, ONE_DAY_IN_MS } from "../../common/utils/date-time";
+import SessionModel from "../../database/models/session.model";
+import jwt from "jsonwebtoken";
+import { config } from "../../config/app.config";
+import { refreshTokenSignOptions, RefreshTPayload, signJwtToken, verifyJwtToken } from "../../common/utils/jwt";
+
+
 
 export class AuthService {
     public async register(registerData:RegisterDto){
@@ -33,4 +39,95 @@ export class AuthService {
             user:newUser
         }
     }
+
+    public async login(loginData:LoginDto){
+        const {email, password, userAgent}=loginData;
+
+        const user=await UserModel.findOne({email});
+
+        if(!user){
+            throw new BadRequestException(
+                "Invalid email or password provided",
+                ErrorCode.AUTH_USER_NOT_FOUND
+            );
+        }
+        const isPasswordValid=await user.comparePassword(password);
+        if(!isPasswordValid){
+            throw new BadRequestException(
+                "Invalid email or password provided",
+                ErrorCode.AUTH_USER_NOT_FOUND
+            );
+        }
+
+        //check if the user enable 2fa return user=null
+
+        const session=await SessionModel.create({
+            userId: user._id, 
+            userAgent
+        });
+
+        const accessToken=signJwtToken({
+            userId: user._id,
+            sessionId: session._id
+        })
+
+        const refreshToken=signJwtToken({
+            sessionId: session._id
+        }, refreshTokenSignOptions)
+
+        return {
+            user,
+            accessToken,
+            refreshToken,
+            mfaRequired: false
+        }
+    }
+
+    public async refreshToken(refreshToken: string) {
+        const { payload } = verifyJwtToken<RefreshTPayload>(refreshToken, {
+            secret: refreshTokenSignOptions.secret,
+        });
+    
+        if (!payload) {
+            throw new UnauthorizedException("Invalid refresh token");
+        }
+    
+        const session = await SessionModel.findById(payload.sessionId);
+        const now = Date.now();
+    
+        if (!session) {
+            throw new UnauthorizedException("Session does not exist");
+        }
+    
+        if (session.expiredAt.getTime() <= now) {
+            throw new UnauthorizedException("Session expired");
+        }
+    
+        const sessionRequireRefresh = session.expiredAt.getTime() - now <= ONE_DAY_IN_MS;
+    
+        if (sessionRequireRefresh) {
+            session.expiredAt = calculateExpirationDate(
+                config.JWT.REFRESH_EXPIRES_IN
+            );
+            await session.save();
+        }
+    
+        const newRefreshToken = sessionRequireRefresh ? signJwtToken(
+                {
+                    sessionId: session._id,
+                },
+                refreshTokenSignOptions
+            )
+        : undefined;
+        const accessToken = signJwtToken({
+            userId: session.userId,
+            sessionId: session._id,
+        });
+        return {
+            accessToken,
+            newRefreshToken,
+        };
+    }
+
+
 }
