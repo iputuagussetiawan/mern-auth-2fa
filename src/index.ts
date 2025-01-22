192f8963926a9c0a71565a35063a682ced405c2d
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-01-22 13:51:22 +0800

    error handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import "dotenv/config"
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import { config } from "./config/app.config";
 import connectDatabase from "./database/database";
+import { errorHandler } from "./middlewares/errorHandler";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler";
 
 const app=express();
 //const BASE_PATH=config.BASE_PATH;
@@ -16,13 +19,16 @@ app.use(cors({
 
 app.use(cookieParser());
 
-app.get("/", (req:Request, res:Response) => {
-    res.status(200).json({
-        success: true,
-        message: "API is running",
-        data: null
+app.post(
+    "/",
+    asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        res.status(HTTPSTATUS.OK).json({
+            message: "Hello Subscribers!!!",
+        });
     })
-})
+);
+
+app.use(errorHandler);
 app.listen(config.PORT, async () => {
     console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
     await connectDatabase();
